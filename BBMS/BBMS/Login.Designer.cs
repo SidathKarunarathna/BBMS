70d91fcb75e9ddc4ef78996a6e65e46f3bec4a5b
unknown <Sidath21325@gmail.com>
2021-08-05 18:25:40 +0530

    login done

---
+++ BBMS/BBMS/Login.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace BBMS
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.login_username = new System.Windows.Forms.TextBox();
+            this.login_password = new System.Windows.Forms.TextBox();
+            this.lbl_forgot = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_register = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(19)))), ((int)(((byte)(51)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(797, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(31, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 64);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "\"Your blood is replaceable. \r\nA life is not.”\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::BBMS.Properties.Resources.welfare_48px;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(622, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(71, 61);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::BBMS.Properties.Resources.user_white;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(536, 177);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 36);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(532, 219);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(268, 2);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::BBMS.Properties.Resources.password_60px_white;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(536, 244);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(37, 36);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(532, 286);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(268, 2);
+            this.panel2.TabIndex = 6;
+            // 
+            // login_username
+            // 
+            this.login_username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(20)))), ((int)(((byte)(53)))));
+            this.login_username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.login_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_username.ForeColor = System.Drawing.Color.Gray;
+            this.login_username.Location = new System.Drawing.Point(577, 186);
+            this.login_username.Name = "login_username";
+            this.login_username.Size = new System.Drawing.Size(220, 21);
+            this.login_username.TabIndex = 7;
+            this.login_username.Text = "User Name";
+            this.login_username.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.login_username.Leave += new System.EventHandler(this.login_username_Leave);
+            // 
+            // login_password
+            // 
+            this.login_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(20)))), ((int)(((byte)(53)))));
+            this.login_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.login_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_password.ForeColor = System.Drawing.Color.Gray;
+            this.login_password.Location = new System.Drawing.Point(577, 254);
+            this.login_password.Name = "login_password";
+            this.login_password.Size = new System.Drawing.Size(220, 21);
+            this.login_password.TabIndex = 8;
+            this.login_password.Text = "Password";
+            this.login_password.Enter += new System.EventHandler(this.login_password_Enter);
+            this.login_password.Leave += new System.EventHandler(this.login_password_Leave);
+            // 
+            // lbl_forgot
+            // 
+            this.lbl_forgot.AutoSize = true;
+            this.lbl_forgot.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_forgot.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_forgot.ForeColor = System.Drawing.Color.White;
+            this.lbl_forgot.Location = new System.Drawing.Point(615, 291);
+            this.lbl_forgot.Name = "lbl_forgot";
+            this.lbl_forgot.Size = new System.Drawing.Size(183, 18);
+            this.lbl_forgot.TabIndex = 9;
+            this.lbl_forgot.Text = "Forgot your password?";
+            this.lbl_forgot.MouseLeave += new System.EventHandler(this.lbl_forgot_MouseLeave);
+            this.lbl_forgot.MouseHover += new System.EventHandler(this.label2_MouseHover);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(480, 108);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(2, 297);
+            this.panel3.TabIndex = 10;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.Transparent;
+            this.btn_login.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.ForeColor = System.Drawing.Color.White;
+            this.btn_login.Location = new System.Drawing.Point(532, 341);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(261, 43);
+            this.btn_login.TabIndex = 11;
+            this.btn_login.Text = "Log in";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.MouseEnter += new System.EventHandler(this.btn_login_MouseEnter);
+            this.btn_login.MouseLeave += new System.EventHandler(this.btn_login_MouseLeave);
+            // 
+            // btn_register
+            // 
+            this.btn_register.BackColor = System.Drawing.Color.Transparent;
+            this.btn_register.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_register.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_register.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_register.ForeColor = System.Drawing.Color.White;
+            this.btn_register.Location = new System.Drawing.Point(532, 412);
+            this.btn_register.Name = "btn_register";
+            this.btn_register.Size = new System.Drawing.Size(261, 43);
+            this.btn_register.TabIndex = 12;
+            this.btn_register.Text = "Register";
+            this.btn_register.UseVisualStyleBackColor = false;
+            this.btn_register.MouseEnter += new System.EventHandler(this.btn_register_MouseEnter);
+            this.btn_register.MouseLeave += new System.EventHandler(this.btn_register_MouseLeave);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = global::BBMS.Properties.Resources._26842__Converted_;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(-59, 177);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(409, 222);
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BBMS.Properties.Resources.back;
+            this.ClientSize = new System.Drawing.Size(836, 513);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.btn_register);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.lbl_forgot);
+            this.Controls.Add(this.login_password);
+            this.Controls.Add(this.login_username);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox login_username;
+        private System.Windows.Forms.TextBox login_password;
+        private System.Windows.Forms.Label lbl_forgot;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_register;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ BBMS/BBMS/Login.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BBMS
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        bool isMouseDown = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                this.SetDesktopLocation(MousePosition.X - 400, MousePosition.Y - 20);
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (login_username.Text=="User Name")
+            {
+                login_username.Text= "";
+                
+
+            }
+            login_username.ForeColor = Color.Red;
+            panel1.BackColor = Color.Red;
+            pictureBox2.Image = null;
+            pictureBox2.Image = BBMS.Properties.Resources.user_red;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void login_username_Leave(object sender, EventArgs e)
+        {
+            if(login_username.Text == "")
+            {
+                login_username.Text = "User Name";
+                login_username.ForeColor = Color.Gray;
+
+            }
+            else
+            {
+                login_username.ForeColor = Color.White;
+            }
+            panel1.BackColor = Color.White;
+            pictureBox2.Image = null;
+            pictureBox2.Image = BBMS.Properties.Resources.user_white;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void login_password_Enter(object sender, EventArgs e)
+        {
+            if (login_password.Text == "Password")
+            {
+                login_password.Text = "";
+                
+
+            }
+            login_password.ForeColor = Color.Red;
+            panel2.BackColor = Color.Red;
+            pictureBox3.Image = null;
+            pictureBox3.Image = BBMS.Properties.Resources.password_60px;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void login_password_Leave(object sender, EventArgs e)
+        {
+            if(login_password.Text == "")
+            {
+                login_password.Text = "Password";
+                login_password.ForeColor = Color.Gray;
+
+            }
+            else
+            {
+                login_password.ForeColor = Color.White;
+            }
+            panel2.BackColor = Color.White;
+            pictureBox3.Image = null;
+            pictureBox3.Image = BBMS.Properties.Resources.password_60px_white;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            lbl_forgot.ForeColor = Color.Red;
+        }
+
+        private void lbl_forgot_MouseLeave(object sender, EventArgs e)
+        {
+            lbl_forgot.ForeColor = Color.White;
+        }
+
+        private void btn_login_MouseLeave(object sender, EventArgs e)
+        {
+            btn_login.BackColor = Color.Transparent;
+            btn_login.ForeColor = Color.White;
+        }
+
+        private void btn_login_MouseEnter(object sender, EventArgs e)
+        {
+            btn_login.BackColor = Color.Red;
+            btn_login.ForeColor = Color.White;
+        }
+
+        private void btn_register_MouseEnter(object sender, EventArgs e)
+        {
+            btn_register.BackColor = Color.Red;
+        }
+
+        private void btn_register_MouseLeave(object sender, EventArgs e)
+        {
+            btn_register.BackColor = Color.Transparent;
+        }
+    }
+}
