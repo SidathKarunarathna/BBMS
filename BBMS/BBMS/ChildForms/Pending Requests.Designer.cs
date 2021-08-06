1cc90861e5f5476b96127696868eda06574704fd
kavindagayashan <kava.gayashan@gmail.com>
2021-08-06 21:42:37 +0530

    Forms

--- BBMS/BBMS/ChildForms/Pending Requests.Designer.cs
+++ BBMS/BBMS/ChildForms/Pending Requests.Designer.cs
@@ -29,21 +29,174 @@ namespace BBMS.ChildForms
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAvailable = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtReqUnits = new System.Windows.Forms.TextBox();
+            this.btnContact = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(41, 64);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(642, 555);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.label2.Location = new System.Drawing.Point(699, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Blood Type";
+            // 
+            // cbType
+            // 
+            this.cbType.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            "A+",
+            "A-",
+            "B+",
+            "B-",
+            "AB+",
+            "AB-",
+            "O+",
+            "O-"});
+            this.cbType.Location = new System.Drawing.Point(899, 122);
+            this.cbType.MinimumSize = new System.Drawing.Size(165, 0);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(197, 26);
+            this.cbType.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.label1.Location = new System.Drawing.Point(699, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Available Units ( ml )";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.label3.Location = new System.Drawing.Point(699, 423);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Requested Units ( ml )";
+            // 
+            // txtAvailable
+            // 
+            this.txtAvailable.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAvailable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAvailable.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtAvailable.Location = new System.Drawing.Point(899, 277);
+            this.txtAvailable.MinimumSize = new System.Drawing.Size(250, 32);
+            this.txtAvailable.Name = "txtAvailable";
+            this.txtAvailable.Size = new System.Drawing.Size(250, 32);
+            this.txtAvailable.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.panel2.Location = new System.Drawing.Point(899, 297);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 2);
+            this.panel2.TabIndex = 12;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.panel3.Location = new System.Drawing.Point(899, 441);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 2);
+            this.panel3.TabIndex = 15;
+            // 
+            // txtReqUnits
+            // 
+            this.txtReqUnits.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtReqUnits.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReqUnits.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtReqUnits.Location = new System.Drawing.Point(899, 421);
+            this.txtReqUnits.MinimumSize = new System.Drawing.Size(250, 32);
+            this.txtReqUnits.Name = "txtReqUnits";
+            this.txtReqUnits.Size = new System.Drawing.Size(250, 32);
+            this.txtReqUnits.TabIndex = 16;
+            // 
+            // btnContact
+            // 
+            this.btnContact.FlatAppearance.BorderSize = 2;
+            this.btnContact.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContact.ForeColor = System.Drawing.Color.Red;
+            this.btnContact.Location = new System.Drawing.Point(912, 587);
+            this.btnContact.Name = "btnContact";
+            this.btnContact.Size = new System.Drawing.Size(184, 41);
+            this.btnContact.TabIndex = 17;
+            this.btnContact.Text = "Contact a Donor";
+            this.btnContact.UseVisualStyleBackColor = true;
             // 
             // Pending_Requests
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.ClientSize = new System.Drawing.Size(1470, 960);
+            this.ClientSize = new System.Drawing.Size(1102, 640);
+            this.Controls.Add(this.btnContact);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.txtReqUnits);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txtAvailable);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Pending_Requests";
             this.Text = "Pending Requests";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAvailable;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtReqUnits;
+        private System.Windows.Forms.Button btnContact;
     }
 }
